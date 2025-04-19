cc86b3f7151ace54d496f8bf156ae82d284d53b6
mshaikh786 <mohsinshaikh786@gmail.com>
2025-04-19 13:33:40 +0300

    cleanup

--- Petsc/poissoneq/job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -N 2
-#SBATCH -t 00:30:00
-#SBATCH -J petsc_ex50
-
-module load cray-petsc
-time srun -n 4 --hint=nomultithread ./ex50 -da_grid_x 16 -da_grid_y 16 -pc_type mg -da_refine 10 -ksp_monitor -ksp_view -log_view
-
--- README.md
+++
@@ -1,6 +0,0 @@
-# RTS Tutotrial
-
-Welcome to Redstart Technology Solutions tutorials repository. Here you will find training material for self-paced trianings and instructor led workshop sessions offered by RTS.
-
-- Adding a test comment
-
