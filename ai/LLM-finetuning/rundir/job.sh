1cb53e82fa98d196bbf9d77c38f3c7349b7471d5
mshaikh786 <mohsinshaikh786@gmail.com>
2025-04-19 13:24:13 +0300

    tested April 19

---
+++ ai/LLM-finetuning/rundir/job.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --job-name=finetune
+#SBATCH --tasks-per-node=1
+#SBATCH --gpus=2
+#SBATCH --gpus-per-node=2
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=100G
+#SBATCH --time=00:10:00
+#SBATCH --account=c2227
+#SBATCH --constraint=rtx2080ti
+
+scontrol show job ${SLURM_JOBID}
+source /ibex/user/shaima0d/miniconda3/bin/activate /ibex/ai/home/shaima0d/KSL_Trainings/hpc-saudi-2024/ds-env
+export CUDA_HOME=${CONDA_PREFIX}
+export OMP_NUM_THREADS=1
+export MAX_JOBS=${SLURM_CPUS_PER_TASK}
+export PYTHONWARNINGS=ignore
+
+export master_ip=$(/bin/hostname -I | cut -d " " -f 2 )
+export MASTER_PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+export nvport=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+
+nvdashboard $nvport &
+echo "ssh -L $nvport:$(/bin/hostname -s):$nvport $USER@glogin.ibex.kaust.edu.sa"
+
+export BASEDIR=cache
+export HF_HOME=${BASEDIR}
+export HF_METRICS_CACHE=${BASEDIR}/metric
+export TRITON_CACHE_DIR=${BASEDIR}/triton
+export TORCH_EXTENSIONS_DIR=${BASEDIR}
+mkdir -p ${HF_METRICS_CACHE} ${TRITON_CACHE_DIR} ${HF_HOME}
+export HUGGINGFACE_HUB_VERBOSITY=debug
+export TOKENIZERS_PARALLELISM=false
+rm -rf ${TORCH_EXTENSIONS_DIR}/*
+cd ../scripts
+
+export DS_CONFIG='ds_configs/ds_config_nozero.json'
+start=$(date +%s)
+srun -l ./wrapper.sh
+end=$(date +%s)
+echo "Elapsed Time: $(($end-$start)) seconds"
+
+cd ..
--- ai/LLM-finetuning/scripts/finetune.py
+++ ai/LLM-finetuning/scripts/finetune.py
@@ -1,6 +1,6 @@
 import os
-from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
-from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelWithLMHead, Trainer, TrainingArguments
+from datasets import load_dataset, DatasetDict
 from transformers import DataCollatorForLanguageModeling
 from transformers import TrainerCallback
 import torch
@@ -8,23 +8,63 @@ import transformers
 
 def main():
 
-    model_name="openai-community/gpt2-xl"   # 1.5B parameters (heavy on optim states)
-    # Load the tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    model_name="openai-community/gpt2"  
+
     # Load dataset from the Hugging Face datasets library
-    dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
+    dataset = load_dataset("pierre-pessarossi/climate-question-answers")
+
+    # Split dataset in train, val, test
+    dataset=dataset.rename_column("instruction", "question")
+    test_dataset=dataset['test']
+    train_dataset=dataset['train'].train_test_split(test_size=0.2,keep_in_memory=False)
+    val_dataset=train_dataset['test']
+    train_dataset.pop('test')  # drop the test column 
+    train_dataset=train_dataset['train'] # Converting the type from DatasetDict to Datasets
+
+    climate_dataset_dict = DatasetDict({
+    'train': train_dataset,
+    'validation': val_dataset,
+    'test': test_dataset
+    })
 
 
     # Tokenize the texts
-    def tokenize_function(examples):
-        return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=512)
+    def preprocess_function(examples):
+    
+        # To catch any anomaly in the curation of dataset i.e. bad datatype either in question or an answer, 
+        # we set the string to a NULL token.
+        batch_size=len(examples['question'])
+        for i in range(batch_size):
+            if examples['question'][i] == None:
+                examples['question'][i] = "[NULL]"
+            if examples['answer'][i] == None:
+                examples['answer'][i] = "[NULL]"
+
+        inputs = [q + " [SEP] " + a for q, a in zip(examples["question"], examples["answer"])]
+        
+        model_inputs = tokenizer(inputs, 
+                                 max_length=200, 
+                                 truncation=True, 
+                                 padding=True, 
+                                 return_tensors="pt")
+    
+    # The "labels" are the tokenized outputs:
+        return model_inputs
 
 
-    # Apply "tokenize_function" to all the examples in iterator object "dataset". 
+    # Load the model
+    model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                trust_remote_code=True)
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+
+
+    # Apply "preprocess_function" to all the examples in iterator object "climate_dataset_dict". 
     # "batched=True" implies passing examples in a batch instead of one at a time
-    tokenized_datasets = dataset.map(tokenize_function, batched=True)
-
+    tokenized_dataset = climate_dataset_dict.map(
+    preprocess_function, 
+    batched=True,batch_size=4,drop_last_batch=True)
 
     # Load the data collator (the batch maker given a dataset)
     data_collator = DataCollatorForLanguageModeling(
@@ -32,38 +72,32 @@ def main():
         mlm=False,  # Causal Lang Model uses a causal (not masked) language model, similar to GPT-2
     )
 
-    # Load the model
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    model.config.use_cache = False
-    model.resize_token_embeddings(len(tokenizer))
-
-    # Define the training arguments
     training_args = TrainingArguments(
-        output_dir='./results',
+        output_dir=os.path.join(os.environ['BASEDIR'],'results'),
         overwrite_output_dir=True,
+        eval_strategy="epoch",
+        learning_rate=3e-4,
+        logging_steps=10,
         num_train_epochs=1,
-        per_device_train_batch_size=1,   # Batch size per GPU
+        per_device_train_batch_size=2,
         save_steps=1000,
         save_total_limit=2,
-        fp16=True, 
-        deepspeed=os.path.join(os.environ['PWD'],os.environ['DS_CONFIG']),  # Path to DeepSpeed config file        
+        deepspeed=os.path.join('ds_configs',os.environ['DS_CONFIG']),  # Path to DeepSpeed config file        
         gradient_checkpointing=True,
         gradient_checkpointing_kwargs={'use_reentrant':True},
         report_to='tensorboard',
-        logging_dir='./logs',
-        logging_steps=10,
+        fp16=True,
+        logging_dir=os.path.join(os.environ['BASEDIR'],"logs"),
         logging_strategy='steps',
         log_level='info',
     )
-
-
     # Initialize the Trainer
     trainer = Trainer(
         model=model,
         args=training_args,
         data_collator=data_collator,
-        train_dataset=tokenized_datasets["train"],
-        eval_dataset=tokenized_datasets["validation"]
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["validation"]
     )
 
 
@@ -72,8 +106,9 @@ def main():
 
 
     # Save the final model and tokenizer
-    model.save_pretrained('./final_model')
-    tokenizer.save_pretrained('./final_model')
+    FINAL_MODEL=os.path.join(os.environ['BASEDIR'],'final_model')
+    model.save_pretrained(FINAL_MODEL)
+    tokenizer.save_pretrained(FINAL_MODEL)
 
 
 if __name__ == "__main__":
